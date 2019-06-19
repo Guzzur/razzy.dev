24e01f6c05e4d5a357f0b69a4708c0099f38a9f2
Felix Razykov <felix@pjoyable.com>
2019-06-19 13:40:49 +0300

    bootstrap and shet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import Lottie from 'lottie-react-web';
-import animation from './animations/lego.json';
+import './App.css';
+import { Row } from 'react-bootstrap';
+
+import TopBar from './components/TopBar'
+
+// import Lottie from 'lottie-react-web';
+// import animation from './animations/lego.json';
 
 function App() {
   return (
-    <div className="screen-block">
-      <div className="construction">
-        <Lottie
-          options={{
-            animationData: animation
-          }}
-        />
-      </div>
-    </div>
+    // <div className="screen-block">
+    //   <div className="construction">
+    //     <Lottie
+    //       options={{
+    //         animationData: animation
+    //       }}
+    //     />
+    //   </div>
+    // </div>
+    <Router>
+      <Row className='w-100 p-0 m-0 justify-content-center'>
+        <TopBar />
+      </Row>
+    </Router>
   );
 }
 

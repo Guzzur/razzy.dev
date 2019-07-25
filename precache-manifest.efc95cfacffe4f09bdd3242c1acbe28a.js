e56ae7d2512c5fdc3f4f6d4a28aabb8aaf5a6065
Felix Razykov <felix@pjoyable.com>
2019-07-26 01:05:22 +0300

    Updates

---
+++ precache-manifest.efc95cfacffe4f09bdd3242c1acbe28a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208985ea8d57c05a8e70a10e7b18178b",
+    "url": "/razzy.dev/index.html"
+  },
+  {
+    "revision": "8ea0c6538ac567d6c9e7",
+    "url": "/razzy.dev/static/css/main.72872252.chunk.css"
+  },
+  {
+    "revision": "d4eda7e1ed5eecd64a3c",
+    "url": "/razzy.dev/static/js/2.73356961.chunk.js"
+  },
+  {
+    "revision": "8ea0c6538ac567d6c9e7",
+    "url": "/razzy.dev/static/js/main.14318d71.chunk.js"
+  },
+  {
+    "revision": "c3b0c903a9ead514a351",
+    "url": "/razzy.dev/static/js/runtime~main.2f6e4653.js"
+  }
+]);

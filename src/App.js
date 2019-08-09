43dec397a3d8dadab26f73f821ef9632b3dfb4d2
Felix Razykov <felix@pjoyable.com>
2019-08-10 02:39:08 +0300

    recreate app, initial commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-
-import "./App.css";
-import { Row, Card, Container } from "react-bootstrap";
-
-import TopBar from "./components/TopBar";
-import About from "./components/About";
-import Projects from "./components/Projects";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Row className="w-100 p-0 m-0 justify-content-center">
-        <TopBar />
-        <Row className="w-100 p-0 m-0" style={{ maxWidth: "1200px" }}>
-          <Card className="w-100 border-0">
-            <Card.Body className="px-0 py-5 m-0">
-              <Container>
-                <Container>
-                  <Container>
-                    <Switch>
-                      <Route path="/about" component={About} />
-                      <Route path="/projects" component={Projects} />
-                    </Switch>
-                  </Container>
-                </Container>
-              </Container>
-            </Card.Body>
-          </Card>
-        </Row>
-      </Row>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
